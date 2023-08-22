19074f7eebbd8e08e7117526d7ae46634ebd11b4
meb-wtag <melvin.eberle@welldev.io>
2023-08-22 09:01:58 +0200

    corrected the migration files
    test for commit

--- db/migrate/20230821124053_create_tasks.rb
+++ db/migrate/20230821124053_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.integer "user_id", null => false
+      t.integer "user_id", :null => false
       t.string "title", :limit => 25
       t.text "content", :limit => 300
       t.date "deadline"
--- db/migrate/20230821124100_create_journals.rb
+++ db/migrate/20230821124100_create_journals.rb
@@ -1,7 +1,7 @@
 class CreateJournals < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.integer "user_id", null => false
+      t.integer "user_id", :null => false
       t.string "title", :limit => 25
       t.text "description", :limit => 300
 
