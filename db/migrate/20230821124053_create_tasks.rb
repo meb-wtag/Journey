7e2b0739ecf2dc8f87c42795b1dfbbc21577b9a0
meb-wtag <melvin.eberle@welldev.io>
2023-08-22 08:50:00 +0200

    created second migration file

--- db/migrate/20230821124053_create_tasks.rb
+++ db/migrate/20230821124053_create_tasks.rb
@@ -1,17 +1,20 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.integer "user_id"
+      t.integer "user_id", null => false
       t.string "title", :limit => 25
-      t.text "content", limt
-      t.text "decription", :limit => 300
-      t.date "date_of_birth" 
-      t.date "joining_date"
+      t.text "content", :limit => 300
+      t.date "deadline"
+      t.integer "importance"
+      t.string "status", :default => "to do"
 
+      
       t.timestamps
     end
+
+    add_index :tasks, :user_id
   end
 
-  add_index :tasks, :user_id
+
 
 end
